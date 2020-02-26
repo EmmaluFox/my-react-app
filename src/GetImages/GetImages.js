11a10752f2145bd0914bed4118ae25a062f2ce17
Emmalu Fox <emmalu.fox@softwire.com>
2020-02-26 10:20:38 +0000

    Initial commit

--- src/GetImages/GetImages.js
+++ src/GetImages/GetImages.js
@@ -19,4 +19,5 @@ function getImages() {
     return urls;
 }
 
+export {getImages};
 export const ImageUrls = getImages();
---
+++ src/Thumbnails/Thumbnail.js
@@ -0,0 +1,8 @@
+﻿﻿import React, {ReactPropTypes as PropTypes, useState} from "react";
+import 'src/PhotoViewer/PhotoViewer.css';
+import {ImageUrls} from 'src/GetImages/GetImages.js';
+import "src/GetImages.GetImages.js"
+
+function Thumbnail() {
+    
+}
